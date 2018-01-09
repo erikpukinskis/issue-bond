065a4efe3e84630d1c5c9cb391d04f6dd7e1bb01
Erik Pukinskis <erik@snowedin.net>
2018-01-08 21:43:06 -0800

    remove dashify

--- issue-bond.js
+++ issue-bond.js
@@ -1,63 +1,10 @@
 var library = require("module-library")(require)
 
-library.define(
-  "dashify",
-  function() {
-    return function dashify(string) {
-      if (typeof string !== 'string') {
-        throw new TypeError('expected a string');
-      }
-
-      var sentence = [];
-      var needsDash = false;
-
-      for(var i=0; i<string.length; i++) {
-        var char = string[i];
-
-        var isNumber = !Number.isNaN(parseFloat(char));
-
-        if (isNumber) {
-          lower = char;
-        } else {
-          var upper = char.toUpperCase();
-          var lower = char.toLowerCase();
-          var isLetter = upper != lower;
-        } 
-
-        var isAlphaNumeric = isNumber || isLetter;
-
-        if (isNumber) {
-          var isUpper = false;
-        } else if (isLetter) {
-          var isUpper = (char == upper);
-        } else {
-          var isUpper = false;
-        }
-
-        if (needsDash && isAlphaNumeric) {
-          sentence.push("-");
-          needsDash = false;
-        } else if (isUpper && sentence.length) {
-          sentence.push("-");
-        }
-
-        if (isAlphaNumeric) {
-          sentence.push(lower);
-        } else if (sentence.length && !isAlphaNumeric) {
-          needsDash = true;
-        }
-      }
-
-      return sentence.join("");
-    };
-  }
-)
-
 
 module.exports = library.export(
   "issue-bond",
-  ["identifiable", "dashify"],
-  function(identifiable, dashify) {
+  ["identifiable"],
+  function(identifiable) {
 
     var outcomes = {}
     var issuers = {}
